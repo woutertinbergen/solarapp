712a4ed9c83150992347c192426c4975011d3cb9
Wouter Tinbergen | INSPIDEE <w.tinbergen@gmail.com>
2014-10-12 10:52:56 +0200

    updated month view

--- solarApp/www/js/controllers.js
+++ solarApp/www/js/controllers.js
@@ -6,6 +6,7 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 	$scope.datum = '2014-09-09'; // startdatum
 	$scope.vorigeZichtbaar = true;
 	$scope.volgendeZichtbaar = false;
+	$scope.vandaag = moment().format('YYYY-MM-DD');
 	$scope.changeDay = function(direction){
 		if(direction=="yesterday"){
 			if(moment($scope.datum).subtract(1, 'days').isAfter(moment('2014-09-08'))){
@@ -19,18 +20,14 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 				$scope.datum = moment($scope.datum).add(1, 'days').format('YYYY-MM-DD').toString();	
 			}else{
 				//console.log('ken niet');
-
-			}
-			
+			}		
 		}
 		$scope.setData($scope.datum);
 	}
-
 	$scope.setData = function(dedatum){
 		$scope.datum = dedatum; // startdatum
 		ServerData.getDay(dedatum).then( 
 			function(data){
-				//console.log(data);
 				var arrayVermogenVandaag 		= data[0].data.split(",");	
 				/* huidige dag */
 				$scope.dag = moment(data[0].day).format('D MMMM');
@@ -139,7 +136,7 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 			}
 		);
 	};
-	$scope.setData();
+	$scope.setData($scope.vandaag);
 }])
 
 .controller('MonthCtrl', ['$scope', '$timeout','ServerData','localStorageService', function($scope, $timeout, ServerData,localStorageService) {
@@ -148,30 +145,52 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 	$scope.datum = '2014-09-08'; // startdatum
 	$scope.vorigeZichtbaar = true;
 	$scope.volgendeZichtbaar = false;
-	$scope.changeDay = function(direction){
-		if(direction=="yesterday"){
-			if(moment($scope.datum).subtract(1, 'days').isAfter(moment('2014-09-08'))){
-				$scope.datum = moment($scope.datum).subtract(1, 'days').format('YYYY-MM-DD').toString();
+
+	$scope.setMonthData = function(givendate){
+		if(angular.isDefined(givendate)){
+			// andere maand --> laatste dag voor totalen.
+			var lastDate = moment(givendate).endOf("month").format("YYYY-MM-DD");
+			console.log(lastDate);
+			$scope.setData(lastDate);
+		}else{
+			console.log('geen datum');
+			// deze maand
+			$scope.setData();
+		}
+	}
+
+	$scope.changeMonth = function(direction){
+		if(direction=="previous"){
+			if(moment($scope.datum).subtract(1, 'month').isAfter(moment('2014-09-08'))){
+				$scope.datum = moment($scope.datum).subtract(1, 'month').format('YYYY-MM-DD').toString();
 
 			}else{
 				//console.log('ken niet eerder');
 			}
+			$scope.setMonthData($scope.datum);
 		}else{
-			if(moment($scope.datum).add(1, 'days').isBefore(moment())){
-				$scope.datum = moment($scope.datum).add(1, 'days').format('YYYY-MM-DD').toString();	
+			if(moment($scope.datum).add(1, 'month').isBefore(moment(),'month') || moment($scope.datum).add(1, 'month').isSame(moment(),'month') ){
+				if(moment($scope.datum).add(1, 'month').isSame(moment(),'month')){
+					console.log('eee');
+					$scope.datum = moment().format('YYYY-MM-DD');
+					$scope.setMonthData();
+				}else{
+					$scope.datum = moment($scope.datum).add(1, 'month').format('YYYY-MM-DD').toString();		
+					$scope.setMonthData($scope.datum);
+				}
 			}else{
 				//console.log('ken niet');
-
 			}
 			
 		}
-		$scope.setData($scope.datum);
+		
 	}
 
 	$scope.setData = function(dedatum){
 		$scope.datum = dedatum; // startdatum
 		ServerData.getDay(dedatum).then( 
 			function(data){
+				console.log(data);
 				var arrayVermogenVandaag 		= data[0].data.split(",");	
 				var arrayEnergyMonth			= data[0].energie_dag.split(",");	
 				/* huidige dag */
@@ -225,18 +244,29 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 		        $scope.overallPeak = localStorageService.get('peakPowerOverall');
 		        
 		        $scope.totaleEnergie=0;
+		        $scope.totaleEnergieDezeMaand = 0;
+		        // haal totaal tot deze maand
+				angular.forEach(localStorageService.get('totalen'),function(value,key){
+					if(moment(key).isBefore(moment(dedatum).format('YYYY-MM'))){
+						$scope.totaleEnergie += parseFloat(value);
+					}else{
+						console.log('komt er bij');
+					}
+				})
+				$scope.maand = moment(dedatum).format("MMMM");
 		        for(var i in arrayEnergyMonth) { 
+		        	$scope.totaleEnergieDezeMaand += parseFloat(arrayEnergyMonth[i]); 
 					$scope.totaleEnergie += parseFloat(arrayEnergyMonth[i]); 
 				}
 				// round it
-				$scope.totaleEnergie = Math.round($scope.totaleEnergie * 100) / 100  
+				$scope.totaleEnergieDezeMaand = Math.round($scope.totaleEnergieDezeMaand * 100) / 100  ;
+				$scope.totaleEnergie = Math.round($scope.totaleEnergie * 100) / 100  ;
 
 				// totaal geld bespaard
 				$scope.getBespaardeEuros = function ()
 				  {
 				    return Math.round($scope.energieprijs * $scope.totaleEnergie * 100) / 100  
 				  };
-				
 				/* huidig vermogen */
 				var index = arrayVermogenVandaag.length -5; // there are 4 emtpy leading zero's...
 				$scope.actueelvermogen = arrayVermogenVandaag[index];
@@ -259,5 +289,58 @@ angular.module('starter.controllers', ['LocalStorageModule'])
 			}
 		);
 	};
-	$scope.setData();
+
+	var init = function(){
+		initApp();
+		$scope.setData($scope.vandaag);
+	}
+
+	var getMonthsBeforeToday = function(datum){
+		var thismonth		= moment(datum).format("YYYY")+'-'+moment(datum).format("MM");;
+				
+		var start = moment("2014-09", "YYYY-MM");
+		var end   = moment(thismonth, "YYYY-MM");
+		var range = moment().range(start, end);
+		var querydates = [];
+		range.by('month', function(moment) {
+	  		if(moment.endOf("month").isAfter(end)){
+	  			// dan vandaag
+	  			querydates.push(datum);
+	  		}else{
+	  			// laatste dag van de maanden ervoor
+	  			querydates.push(moment.endOf("month").format('YYYY-MM-DD'));
+	  		}	
+		});
+		return querydates;	
+	};
+
+	var initApp = function(){
+		// get all totals:
+		// -- maandtotalen sinds startdatum
+
+		// startmaand is 2014-09
+		// haal alle laatste dagen op per maand sinds 2014-09
+		// tel de totalen door elk dagtotaal op te tellen
+		// zet in object en store in local storage op index
+
+		var today 			= moment().format('YYYY-MM-DD');
+		var daysToRetrieve = getMonthsBeforeToday(today);
+		$scope.totals = {};
+		daysToRetrieve.forEach(function(dag) {
+		    ServerData.getDay(dag).then( 
+		    	function(data){
+			    	var dezemaandtotaal=0;
+			    	var arrayEnergyMonth	= data[0].energie_dag.split(",");	
+			    	for(var i in arrayEnergyMonth) { 
+						dezemaandtotaal += parseFloat(arrayEnergyMonth[i]); 
+					}
+
+					$scope.totals[moment(dag).format('YYYY-MM')] = dezemaandtotaal;	
+					localStorageService.set('totalen',JSON.stringify($scope.totals));
+		    	}
+		    );
+		});
+	}
+	initApp();
+	init();
 }]);
